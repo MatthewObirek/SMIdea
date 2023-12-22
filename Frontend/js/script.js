d4419a1b745bb6df521cf7c5c7468bc6e783f693
MatthewObirek <matthewobirek@gmail.com>
2023-12-22 08:59:20 -0800

    Added posting functionality

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const outputDiv = document.getElementById('output');
   const addNewDiscussion = document.getElementById('addNewDiscussion');
 
-
+  var dialogWindowOpen = false;
   // Backend Post Retrieval 
   runButton.addEventListener('click', async () => {
     try {
@@ -23,14 +23,51 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
+
   // For creating new discussions
   addNewDiscussion.addEventListener('click', () => {
+    // elem style
+    if(dialogWindowOpen)
+      return;
+    dialogWindowOpen = true;
     const node = document.createElement('div');
     node.style = 'background: grey;';
     node.className = 'box stack-top';
-    node.innerHTML = 'goodby';
+
+    // Input elem
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'What are your Ideas?';
+    node.appendChild(input);
+
+    // Button elem
+    const button = document.createElement('button');
+    button.innerText = 'Submit';
+    button.addEventListener('click', () => {
+      addPost('user', input.value);
+      dialogWindowOpen = false;
+      node.remove();
+    });
+    node.appendChild(button);
+    // add Node to Container
     container.appendChild(node);
   });
   // who is there
 
+
+  function addPost(username, postContent){
+    const post = document.createElement('div');
+    
+    post.className = 'post-container';
+
+    const user = document.createElement('p');
+    user.innerText = 'Username: ' + username;
+
+    const content = document.createElement('p');
+    content.innerText = postContent;
+
+    post.appendChild(user);
+    post.appendChild(content);
+    container.appendChild(post);
+  }
 });
